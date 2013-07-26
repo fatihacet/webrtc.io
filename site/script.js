adc8302f7737e48d7fec388c5240a4eb0409437f
Fatih Acet <acetfatih@gmail.com>
2013-07-26 17:41:32 +0300

    Removed commented codes.

--- site/script.js
+++ site/script.js
@@ -56,123 +56,6 @@ function removeVideo(socketId) {
   }
 }
 
-// function addToChat(msg, color) {
-//   var messages = document.getElementById('messages');
-//   msg = sanitize(msg);
-//   if(color) {
-//     msg = '<span style="color: ' + color + '; padding-left: 15px">' + msg + '</span>';
-//   } else {
-//     msg = '<strong style="padding-left: 15px">' + msg + '</strong>';
-//   }
-//   messages.innerHTML = messages.innerHTML + msg + '<br>';
-//   messages.scrollTop = 10000;
-// }
-
-// function sanitize(msg) {
-//   return msg.replace(/</g, '&lt;');
-// }
-
-// function initFullScreen() {
-//   var button = document.getElementById("fullscreen");
-//   button.addEventListener('click', function(event) {
-//     var elem = document.getElementById("videos");
-//     //show full screen
-//     elem.webkitRequestFullScreen();
-//   });
-// }
-
-function initNewRoom() {
-  var button = document.getElementById("newRoom");
-
-  button.addEventListener('click', function(event) {
-
-    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
-    var string_length = 8;
-    var randomstring = '';
-    for(var i = 0; i < string_length; i++) {
-      var rnum = Math.floor(Math.random() * chars.length);
-      randomstring += chars.substring(rnum, rnum + 1);
-    }
-
-    window.location.hash = randomstring;
-    location.reload();
-  })
-}
-
-
-// var websocketChat = {
-//   send: function(message) {
-//     rtc._socket.send(message);
-//   },
-//   recv: function(message) {
-//     return message;
-//   },
-//   event: 'receive_chat_msg'
-// };
-
-// var dataChannelChat = {
-//   send: function(message) {
-//     for(var connection in rtc.dataChannels) {
-//       var channel = rtc.dataChannels[connection];
-//       channel.send(message);
-//     }
-//   },
-//   recv: function(channel, message) {
-//     return JSON.parse(message).data;
-//   },
-//   event: 'data stream data'
-// };
-
-// function initChat() {
-//   var chat;
-
-//   if(rtc.dataChannelSupport) {
-//     console.log('initializing data channel chat');
-//     chat = dataChannelChat;
-//   } else {
-//     console.log('initializing websocket chat');
-//     chat = websocketChat;
-//   }
-
-//   var input = document.getElementById("chatinput");
-//   var toggleHideShow = document.getElementById("hideShowMessages");
-//   var room = window.location.hash.slice(1);
-//   var color = "#" + ((1 << 24) * Math.random() | 0).toString(16);
-
-//   toggleHideShow.addEventListener('click', function() {
-//     var element = document.getElementById("messages");
-
-//     if(element.style.display === "block") {
-//       element.style.display = "none";
-//     }
-//     else {
-//       element.style.display = "block";
-//     }
-
-//   });
-
-//   input.addEventListener('keydown', function(event) {
-//     var key = event.which || event.keyCode;
-//     if(key === 13) {
-//       chat.send(JSON.stringify({
-//         "eventName": "chat_msg",
-//         "data": {
-//           "messages": input.value,
-//           "room": room,
-//           "color": color
-//         }
-//       }));
-//       addToChat(input.value);
-//       input.value = "";
-//     }
-//   }, false);
-//   rtc.on(chat.event, function() {
-//     var data = chat.recv.apply(this, arguments);
-//     console.log(data.color);
-//     addToChat(data.messages, data.color.toString(16));
-//   });
-// }
-
 
 function init() {
   if(PeerConnection) {
@@ -206,9 +89,6 @@ function init() {
     console.log('remove ' + data);
     removeVideo(data);
   });
-  initFullScreen();
-  initNewRoom();
-  initChat();
 }
 
 window.onresize = function(event) {
