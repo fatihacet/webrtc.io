4f570a32ed1ed5a93326209a9f16d79011f68b51
Fatih Acet <acetfatih@gmail.com>
2013-07-26 18:18:50 +0300

    Removed chat related logic.

--- site/server.js
+++ site/server.js
@@ -26,31 +26,3 @@ app.get('/script.js', function(req, res) {
 app.get('/webrtc.io.js', function(req, res) {
   res.sendfile(__dirname + '/webrtc.io.js');
 });
-
-/*
-webRTC.rtc.on('chat_msg', function(data, socket) {
-  var roomList = webRTC.rtc.rooms[data.room] || [];
-
-  for (var i = 0; i < roomList.length; i++) {
-    var socketId = roomList[i];
-
-    if (socketId !== socket.id) {
-      var soc = webRTC.rtc.getSocket(socketId);
-
-      if (soc) {
-        soc.send(JSON.stringify({
-          "eventName": "receive_chat_msg",
-          "data": {
-            "messages": data.messages,
-            "color": data.color
-          }
-        }), function(error) {
-          if (error) {
-            console.log(error);
-          }
-        });
-      }
-    }
-  }
-});
-*/
